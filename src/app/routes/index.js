8134e00c9d9511d6ab4584b525ff2c5137e35791
Dev-as-sayed <assayed277@gmail.com>
2025-02-09 23:12:08 +0600

    connect teacher router with main route on this commit

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { AnnouncementRouter } from "../module/Anousment/anousment.routes.js";
+import { TeacherRouter } from "../module/Teacher/teacher.routes.js";
 // import AnnouncementRouter from "../module/Anousment/anousment.routes.js";
 const router = express.Router();
 
-const moduleRoutes = [{ path: "/announcement", route: AnnouncementRouter }];
+const moduleRoutes = [
+  { path: "/announcement", route: AnnouncementRouter },
+  { path: "/teacher", route: TeacherRouter },
+];
 
 moduleRoutes.forEach((el) => router.use(el.path, el.route));
 
